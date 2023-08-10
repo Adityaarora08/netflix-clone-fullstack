ea00d42a317d465a57597b856813ad8323307e12
Aditya Arora <rituishuaddy@gmail.com>
2023-08-11 04:04:01 +0530

    changes

---
+++ src/components/PlansScreen.jsx
@@ -0,0 +1,184 @@
+import { useEffect, useState } from "react";
+import db from "./firebasefile";
+import classes from "./PlansScreen.module.css";
+import { loadStripe } from "@stripe/stripe-js";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice"; 
+
+// Plans component to display all plans fetched from database
+function PlansScreen() {
+  const [products, setProducts] = useState([]);
+  const user = useSelector(selectUser);
+  const [subscription, setSubscription] = useState(null);
+  useEffect(() => {
+    db.collection("customers")
+      .doc(user?.uid)
+      .collection("subscriptions")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach(async (subscription) => {
+          setSubscription({
+            role: subscription.data().role,
+            current_period_end: subscription.data().current_period_end.seconds,
+            current_period_start:
+              subscription.data().current_period_start.seconds,
+          });
+        });
+      });
+  }, [user?.uid]);
+  // console.log(subscription);
+  useEffect(() => {
+    db.collection("products")
+      .where("active", "==", true)
+      .get()
+      .then((querySnapshot) => {
+        const products = {};
+        querySnapshot.forEach(async (productDoc) => {
+          products[productDoc.id] = productDoc.data();
+          const priceSnap = await productDoc.ref.collection("prices").get();
+          priceSnap.docs.forEach((price) => {
+            products[productDoc.id].prices = {
+              priceId: price.id,
+              priceData: price.data(),
+            };
+          });
+        });
+        setProducts(products);
+      });
+  }, []);
+  // console.log(products);
+  // console.log(user?.uid);
+  const unsubscribe = async () => {
+    setSubscription({
+      role: 'Nan',
+      current_period_end: null,
+      current_period_start: null,
+    });
+    const docRef = await db.collection("customers")
+      // .doc(user?.uid)
+      // .collection("subscriptions")
+      // .add(
+      //   {
+      //     role: 'Nan',
+      //     current_period_end: 0,
+      //     current_period_start: 0,
+      //   }
+      // )
+      // docRef.onSnapshot((snap) => {
+      //   const { error, url } = snap.data();
+      //   // console.log(snap)
+      //   if (error) {
+      //     // Show an error to the customer and
+      //     // inspect your Cloud Function logs in the Firebase console.
+      //     alert(`An error occured: ${error.message}`);
+      //   }
+      //   if (url) {
+      //     //  Stripe Checkout URL, redirect.
+      //     window.location.assign(url);
+      //   }
+      // });
+      const snapshot = await docRef
+      .doc(user?.uid)
+      .collection("subscriptions")
+      .get();
+      snapshot.forEach((snap) => {
+        snap.ref.delete()
+        // console.log(snap);
+      });
+  }
+  const loadCheckout = async (priceId) => {
+    // console.log(priceId)
+    // console.log("Ashish" + user.uid);
+
+    // const docRef = await db
+    //   .collection("customers")
+    //   .doc(user.uid)
+    //   .collection("checkout_session")
+    //   .add({
+    //     price: priceId,
+    //     success_url: window.location.origin,
+    //     cancel_url: window.location.origin,
+    //   });
+    // docRef.onSnapshot(async (snap) => {
+    //   console.log(snap.data());
+    //   const { error, sessionId } = snap.data();
+
+    //   if (error) {
+    //     alert(`An error occured: ${error.message}`);
+    //   }
+
+    //   if (sessionId) {
+    //     const stripe = await loadStripe(
+    //       "rk_test_51NLTAuSHyag4YdKgptKPjFUhyjpIcYIQbX5tT2B9WBggjORofkcubVI1vSItC9SxXAsSmkSBi4K0k5UT7tDY8MxL00zC8Orhh9"
+    //     );
+    //     stripe.redirectToCheckout({sessionId})
+    //   }
+    // });
+    const docRef = await db
+      .collection("customers")
+      .doc(user.uid)
+      .collection("checkout_sessions")
+      .add({
+        price: priceId,
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      });
+      // console.log(window.location.origin);
+    // Wait for the CheckoutSession to get attached by the extension
+    docRef.onSnapshot((snap) => {
+      const { error, url } = snap.data();
+      if (error) {
+        // Show an error to the customer and
+        // inspect your Cloud Function logs in the Firebase console.
+        alert(`An error occured: ${error.message}`);
+      }
+      if (url) {
+        //  Stripe Checkout URL, redirect.
+        window.location.assign(url);
+      }
+    });
+  };
+  return (
+    <div className={classes.plansScreen}>
+      {subscription && (
+        <p>
+          Plan renews on :{" "}
+          {subscription?.current_period_end!==null?
+          new Date(
+            subscription?.current_period_end * 1000
+          ).toLocaleDateString():'No active plan'
+          }
+        </p>
+      )}
+      <h2>Popular Plans :</h2>
+      {Object.entries(products).map(([productId, productData]) => {
+        const isCurrentPackage = productData.name?.includes(subscription?.role);
+        // console.log(subscription);
+        return (
+          <div
+            className={
+              isCurrentPackage
+                ? classes.plansScreen__plan__disabled
+                : classes.plansScreen__plan
+            }
+          >
+            <div className={classes.plansScreen__info}>
+              <h5>{productData.name}</h5>
+              <h6>{productData.description}</h6>
+            </div>
+            <button
+              className={classes.plansScreen__button}
+              onClick={() => {
+                isCurrentPackage?unsubscribe():loadCheckout(productData.prices.priceId
+              )}}
+            >
+              {isCurrentPackage ? "Unsubscribe" : "Subscribe"}
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default PlansScreen;
---
+++ src/components/Signin.jsx
@@ -0,0 +1,82 @@
+import React, { useRef, useState } from "react";
+import classes from "./Signin.module.css";
+import { auth } from "./firebasefile";
+
+//sign in/ sign up page for creating new user or logging into existing user 
+function Signin() {
+  const name = useRef();
+  const emailRef = useRef();
+  const passRef = useRef();
+  const [signUp, setSignUp] = useState(false);
+
+  const signInHandler = (event) => {
+    event.preventDefault();
+    auth
+      .signInWithEmailAndPassword(emailRef.current.value, passRef.current.value)
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        console.log("button clicked")
+        alert(error.message);
+      });
+  };
+  const register = (event) => {
+    event.preventDefault();
+    console.log(emailRef.current.value);
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  return (
+    <div className={classes.singinScreen}>
+      {signUp?(
+        <form>
+        <h1>Sign Up</h1>
+        <input ref={emailRef} placeholder="Email" type="email" />
+        <input ref={passRef} placeholder="Create password" type="password" />
+        <input ref={name} placeholder="Full Name" type="text" />
+        <button onClick={register} type="submit">
+          Sign Up
+        </button>
+        <h4>
+          <span className={classes.singinScreen__gray}>Already on Netflix? </span>
+          <span className={classes.singinScreen__link} onClick={()=>{
+            setSignUp(false);
+          }}>
+            Sign In now.
+          </span>
+        </h4>
+      </form>
+       ):(
+        <form>
+        <h1>Sign In</h1>
+        <input ref={emailRef} placeholder="Email" type="email" />
+        <input ref={passRef} placeholder="Password" type="password" />
+        <button onClick={signInHandler} type="submit">
+          Sign In
+        </button>
+        <h4>
+          <span className={classes.singinScreen__gray}>New to Netflix? </span>
+          <span className={classes.singinScreen__link} onClick={()=>{
+            setSignUp(true);
+          }}>
+            Sign Up now.
+          </span>
+        </h4>
+      </form>
+      )} 
+    </div>
+  );
+}
+
+export default Signin;
